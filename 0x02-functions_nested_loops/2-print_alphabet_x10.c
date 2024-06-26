6b92f3bfcf6e56a93e24d36376523e9c06027a28
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-06-25 18:34:15 -0700

    #

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,10 @@
  * print_alphabet_x10 - prints alphabet 10x
  * Return: alphabet
  */
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	for (i = 0; i < 10; i++)
 	{
 		printf("abcdefghijklmnopqrstuvwxyz\n");
