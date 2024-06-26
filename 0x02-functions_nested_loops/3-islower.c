f683e872bc2805c56183bf08620feb37947f3c93
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-06-25 19:08:29 -0700

    #

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,11 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * Return: 0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _islower(int c)
