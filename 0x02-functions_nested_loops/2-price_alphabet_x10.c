64c7efc47cc2ec1f126883aae1b0c474c21184c9
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-06-25 18:20:29 -0700

    #

---
+++ 0x02-functions_nested_loops/2-price_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet 10x
+ * Return: alphabey
+ */
+void print_alphabet(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+
+	}
+}
