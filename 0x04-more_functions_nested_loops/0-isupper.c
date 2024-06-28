51042f2a694ff9903e937d50d21998851b5fc62a
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-06-28 13:30:47 -0700

    #

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Returns 1 if c is uppercase & Returns 0 otherwise 
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	return (isupper(c));
+}
