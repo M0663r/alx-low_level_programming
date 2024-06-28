c5606a091c8bad468b5af4f57971fee53a0e1e39
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-06-28 13:35:08 -0700

    #

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - Returns 1 if c is a digit & Returns 0 otherwise 
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	return (isdigitc));
+}
