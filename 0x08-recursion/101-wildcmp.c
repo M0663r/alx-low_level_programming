5113d1a4a9f1a88223ba8709cd55a41ae003b2d9
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-07-10 21:05:22 -0700

    #

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,28 +10,28 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '\0')
-    {
-        return (*s1 == '\0');
-    }
+	if (*s2 == '\0')
+	{
+		return (*s1 == '\0');
+	}
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '\0')
-        {
-            return 1;
-        }
-        if (*s1 == '\0')
-        {
-            return wildcmp(s1, s2 + 1);
-        }
-        return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
-    }
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+		{
+			return (1);
+		}
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
 
-    if (*s1 == *s2)
-    {
-        return wildcmp(s1 + 1, s2 + 1);
-    }
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
 
-    return 0;
+	return (0);
 }
