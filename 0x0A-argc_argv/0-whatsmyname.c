2715a54b036d4eea9f1822443f80cc3cdc2136f9
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-07-17 15:52:21 -0700

    #

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -13,3 +13,15 @@ int print_program_name(int argc __attribute__((unused)), char *argv[])
 	printf("%s\n", argv[0]);
 	return (0);
 }
+
+/**
+ * main - Entry point
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	return print_program_name(argc, argv);
+}
