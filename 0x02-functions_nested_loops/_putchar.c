ee58fb3b65a71cd6f845cad97d21e0d3541413c4
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-07-25 06:15:07 -0700

    #

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-#include "main.h"
 
-int _putchar(char c)
+/**
+ * main - prints _putchar followed by a new line
+ * Return: 0
+ */
+
+int main(void)
 {
-    return write(1, &c, 1);
+	printf("_putchar\n");
+	return (0);
 }
